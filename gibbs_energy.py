3dff65ebc85b21fadac611d7d3bab46793bd8667
Adam Šrut <43886886+juicce@users.noreply.github.com>
2018-10-05 17:31:08 +0200

    Add files via upload

---
+++ gibbs_energy.py
@@ -0,0 +1,84 @@
+#! /usr/bin/python3
+
+"""The script requires Python3, numpy library and Unix OS, 
+run the script from a folder with your .log files """
+
+""" First, the script creates a list of files and a list of file names."""
+
+files = []
+names = []
+j = 1
+
+while j != 0: # Asks for next file until you type 'end'.
+    entry = input("Please, type a name of .log file or type \'end\' for termination and press Enter: \n")
+    if 'end' in entry and len(entry) <= 5: # Trying to cover all options for typing 'end'
+        j = 0
+    else:
+        files.append(entry)
+        name = entry.replace('.log','')
+        names.append(name)
+
+""""Second, script open each file, find the line with 'Sum of electronic and thermal Free Energies' 
+and save energy in Hartree to energies_au list."""
+
+import re
+
+energies_au = []
+NumRegex = re.compile(r'-\d+\.\d+') #Regex for negative float
+
+for file in files:
+    temp_file = open(file,'r')
+    for line in temp_file:
+        if 'Sum of electronic and thermal Free' in line:
+            gibbs_energy = line
+        else:
+            pass
+    temp_file.close()
+    mo = NumRegex.search(gibbs_energy)
+    num = float(mo.group())
+    energies_au.append(num)
+
+"""Now the script converts energies in Hartree to a new list of energies in eV and kJ/mol. 
+It also creates a list of variances between an energy of fist file and energies of the others."""
+
+import numpy as np
+Hartree_eV = 27.211385
+
+def eV_convert(eng):
+    
+    """Converts energies in given list (eng) from Hartree to eV, return a numpy.array"""
+    
+    loc1 = np.array(eng)
+    loc2 = loc1 * Hartree_eV
+    return loc2
+
+def variance(eng):
+    
+    """Takes a list of energies and counts a variance between first energy in the list, 
+    returns a list"""
+    
+    loc1 = []
+    for i in eng:
+        loc2 = eng[0] - i
+        loc1.append(loc2)
+    return loc1
+
+energies_eV = eV_convert(energies_au)
+variances = variance(energies_eV)
+
+"""At last, the script writes all the data to a new file named gibbs_energy.txt"""
+
+file = open('gibbs_energy.txt','w')
+content = "Name,Energy [eV],Eng. Var. [eV]\n"
+
+for i in range(len(names)):
+    line = str(names[i]) + ',' + str(energies_eV[i]) + ',' + str(variances[i]) + '\n'
+    content += line
+
+file.write(content)
+
+    
+    
+    
+
+
